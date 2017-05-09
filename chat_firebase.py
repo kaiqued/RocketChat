d0191a727167d44ec63df87315b53c39d11a00cc
gabriellm1 <gabriellopesmonteiro41@gmail.com>
2017-05-09 15:52:43 -0300

    Arquivo debugado
    retirada do que não estava funcionando, tornando arquivo funcional(login e post)

--- chat_firebase.py
+++ chat_firebase.py
@@ -1,99 +1,34 @@
-from firebase import firebase
 from multiprocessing import Process
+from firebase import firebase
 from sseclient import SSEClient
 import json
 import time
-import properties as prop
 
-
-class Chat(object):
-
-	def __init__(self):
-		print ("\n\n\n--- Rocket Chat ---")
-
-
-	def firebase_data(self):
-		# Firebase URL
-		fbase = firebase.FirebaseApplication(prop.https://rocket-messenger.firebaseio.com/, None)
-
-		# Le o JSON
-		result = fbase.get('', None)
-		print (result)
-		print ("\n\n\n")
-
-		
-		# Le o nome do grupo
-		result_grupo = fbase.get('Grupo', None)
-		print (result_grupo)
-		print ("\n\n\n")
-
-
-		# Le o JSON
-		result = fbase.get('', None)
-		print (result)
-		print ("\n\n\n")
+FIREBASE_URL = "https://rocket-messenger.firebaseio.com/"
 
 
 
-	def firebase_chat(self):
-		
-		server_sent = SSEClient(prop.https://rocket-messenger.firebaseio.com/ + "Messages.json")
+if __name__ == "__main__":
+    print(__name__)
+    time.sleep(1)
+    username = input("Input your username: ")
+    fb = firebase.FirebaseApplication(FIREBASE_URL,  None)
 
-		print("\n\nFirebase Server - %s" % (prop.https://rocket-messenger.firebaseio.com/ + "Messages.json"))
+    # Avisa que o usuário logou
+    fb.post("rocket-messenger",
+            {"name": username,
+             "message": "Joined the chat",
+             ".priority": time.time() * 1000
+            })
 
+    # Posta mensagem do usuário
+    while (True):
+        message = input("")
+        print("\n")
+        fb.post("rocket-messenger",
+                {
+                    "name": username,
+                    "message": message,
+                    ".priority": time.time() * 1000
+                })
 
-		for new_msg in server_sent:
-			msg_data = json.loads(new_msg.data)
-			# printa nova mensagem
-
-			if msg_data is None:
-				continue
-
-			# Le mensagens ja existentes
-			if msg_data["path"] == "/": 
-				print("\nWhen you weren't there ... \n")
-				for (nodeid, message) in msg_data["data"].items():
-					try:
-						print("%s says: %s\n" % (msg["name"], msg["message"]))
-						
-					except:
-						pass
-
-			# #New Message
-			else: 
-				try:
-					print("%s says: %s\n" % (msg_data["data"]["name"], msg_data["data"]["message"]))
-					# print "\n"
-				except:
-					pass
-
-	
-
-if __name__ == '__main__':
-	rocket_firebase = FirebaseApp()
-
-	# acompanhar mudanças
-	chat_app = Process(target=rocket_firebase.firebase_chat)
-	chat_app.start()
-
-	time.sleep(1)
-	username = input("Username: ")
-	fb = firebase.FirebaseApplication(prop.https://rocket-messenger.firebaseio.com/, None)
-
-	# Posta mensagem inicial
-	fb.post('Messages',
-			{"name": username,
-			 "message": "Entrou na discussão",
-			 ".priority": time.time() * 1000
-			})
-
-	# Posta mensagens novoas
-	while (True):
-		message = input(" ")
-		print("\n")
-		fb.post('Messages',
-				{
-					"name": username,
-					"message": message,
-					".priority": time.time() * 1000
-				})
