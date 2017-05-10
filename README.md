94e1096002092901a98855dbb40662dd4488ff16
kaiqued <kaiqued@al.insper.edu.br>
2017-05-10 00:54:16 -0300

    Melhorei o design do chat, não modifiquei o arquivo chat_firebase.py

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 4 de maio de 2017:
+    
 primeiramente may the force be with you
 Alteramos o nome do aplicativo para :"RocketChat"
--- chat_firebase.py
+++ chat_firebase.py
@@ -4,31 +4,27 @@ from sseclient import SSEClient
 import json
 import time
 
+
 FIREBASE_URL = "https://rocket-messenger.firebaseio.com/"
-
-
 
 if __name__ == "__main__":
     print(__name__)
     time.sleep(1)
-    username = input("Input your username: ")
     fb = firebase.FirebaseApplication(FIREBASE_URL,  None)
 
     # Avisa que o usuário logou
     fb.post("rocket-messenger",
-            {"name": username,
+            {"name": Logins[0],
              "message": "Joined the chat",
              ".priority": time.time() * 1000
             })
 
     # Posta mensagem do usuário
     while (True):
-        message = input("")
         print("\n")
         fb.post("rocket-messenger",
                 {
-                    "name": username,
-                    "message": message,
+                    "name": Logins[0],
+                    "message": texto,
                     ".priority": time.time() * 1000
                 })
-
