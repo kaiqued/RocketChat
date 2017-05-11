42704ee69409f1587bdf2946cba0683ec64a8318
hugogr99 <hugoncalves@outlook.com>
2017-05-11 15:46:09 -0300

    Teste de mudança no ícone

---
+++ kk.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+tk = Tk()
+tk.wm_iconbitmap(r'C:\Users\hugogr\Downloads\Rocket-Filled-50.ico')
+lab = Label(tk, text='Window with transparent icon.')
+lab.pack()
+tk.mainloop()
