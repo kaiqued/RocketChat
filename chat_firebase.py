2ffc3240a7e8120a8061ffa1d9324c447fd6c6c9
gabriellm1 <gabriellopesmonteiro41@gmail.com>
2017-05-17 09:18:44 -0300

    adicionando metodo de senha e registro de conta

--- chat_firebase.py
+++ chat_firebase.py
@@ -2,22 +2,27 @@ from multiprocessing import Process
 from firebase import firebase
 from sseclient import SSEClient
 import json
-import time
+
 
 FIREBASE_URL = "https://rocket-messenger.firebaseio.com/"
 fb = firebase.FirebaseApplication(FIREBASE_URL,  None)
 
 class Rocket_Chat():
 
-    def __init__(self,login):
+    def __init__(self,login,password):
         self.nome=login
-
+        self.senha=password
+        fb.post("data",
+                {"name": self.nome,
+                 "password": self.senha
+                })
+    
+         
     def post(self):
         # Avisa que o usuário logou
         fb.post("rocket-messenger",
                 {"name": self.nome,
-                 "message": "Joined the chat",
-                 ".priority": time.time() * 1000
+                 "message": "Joined the chat"
                 })
 
     def send(self,mensagem):
@@ -25,8 +30,7 @@ class Rocket_Chat():
         fb.post("rocket-messenger",
                 {
                     "name": self.nome,
-                    "message": self.mes,
-                    ".priority": time.time() * 1000
+                    "message": self.mes
                 })
 
 
@@ -35,6 +39,18 @@ def receive():
     reading = fb.get("https://rocket-messenger.firebaseio.com/","rocket-messenger")
     return reading
 
+def checa_login_senha():
+	login = username.get()
+	senha = password.get()
+	saves = fb.get("https://rocket-messenger.firebaseio.com/","data")
+	for lg in data:
+		name=saves[lg]["name"]
+		password=saves[lg]["password"]
+		if name==login and password==senha:
+			Tinicio.destroy()
+		else:
+			lb["fg"]="red"
+			lb["text"]="Login não existe!"
 
 
 
